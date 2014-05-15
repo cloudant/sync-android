ced9cbf7a1777f3b67ed09b79dbfcdd96dacc899
Tom Blench <tom@blench.org>
2014-05-15 14:03:18 +0100

    Cleanup:
    * Remove unused variables
    * Add licence headers

--- src/test/java/com/cloudant/sync/datastore/MultipartAttachmentWriterTests.java
+++ src/test/java/com/cloudant/sync/datastore/MultipartAttachmentWriterTests.java
@@ -1,3 +1,17 @@
+/**
+ * Copyright (c) 2014 Cloudant, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+
 package com.cloudant.sync.datastore;
 
 import com.cloudant.sync.util.TestUtils;
@@ -11,7 +25,6 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
 import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -79,8 +92,6 @@ public class MultipartAttachmentWriterTests {
             }
             mpw.close();
 
-            ByteArrayOutputStream bos = new ByteArrayOutputStream();
-
             int amountRead = 0;
             int totalRead = 0;
 
@@ -113,8 +124,6 @@ public class MultipartAttachmentWriterTests {
             Attachment att0 = new UnsavedFileAttachment(new File("fixture", "bonsai-boston.jpg"), "image/jpeg");
             mpw.addAttachment(att0);
             mpw.close();
-
-            ByteArrayOutputStream bos = new ByteArrayOutputStream();
 
             int amountRead = 0;
             int totalRead = 0;
