e435a4579c36192ff6cd6dfed10436b33b8ae5d4
Al Finkelstein <alfinkel@us.ibm.com>
2014-12-10 13:39:25 -0600

    Provide Spock test/spec proof of concept
    - Create simple Spock spec that tests Java code
    - Update build.gradle(s) to include Spock dependencies

---
+++ sync-core/src/main/java/com/cloudant/sync/query/POCCalculation.java
@@ -0,0 +1,11 @@
+package com.cloudant.sync.query;
+
+public class POCCalculation {
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    int subtract(int a, int b) {
+        return (a - b);
+    }
+}
