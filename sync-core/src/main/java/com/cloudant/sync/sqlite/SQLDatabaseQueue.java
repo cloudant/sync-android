86ec541674f99bc63ef0eb56194f02775ed1407f
Esteban Laver <emlaver@us.ibm.com>
2015-04-10 16:59:44 -0400

    - Add SQLQueue constructor with additional parameter for key provider.  This is required for passing key to SQLCipher-based databases.

--- sync-core/src/main/java/com/cloudant/sync/sqlite/SQLDatabaseQueue.java
+++ sync-core/src/main/java/com/cloudant/sync/sqlite/SQLDatabaseQueue.java
@@ -14,6 +14,8 @@
 
 package com.cloudant.sync.sqlite;
 
+import com.cloudant.sync.sqlite.android.encryption.KeyProvider;
+
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.concurrent.Callable;
@@ -42,6 +44,22 @@ public class SQLDatabaseQueue {
      */
     public SQLDatabaseQueue(String filename) throws IOException {
         this.db = SQLDatabaseFactory.createSQLDatabase(filename);
+        queue.submit(new Runnable() {
+            @Override
+            public void run() {
+                db.open();
+            }
+        });
+    }
+
+    /**
+     * Creates an SQLQueue for the SQLCipher-based database specified.
+     * @param filename The file where the database is located
+     * @param provider The key provider object that contains the user-defined SQLCipher key
+     * @throws IOException If a problem occurs creating the database
+     */
+    public SQLDatabaseQueue(String filename, KeyProvider provider) throws IOException {
+        this.db = SQLDatabaseFactory.createSQLDatabase(filename, provider);
         queue.submit(new Runnable() {
             @Override
             public void run() {
