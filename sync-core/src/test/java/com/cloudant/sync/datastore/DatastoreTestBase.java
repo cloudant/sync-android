adbbad83a65f7945f0a7efd339b9e530d0d85c86
Esteban Laver <emlaver@us.ibm.com>
2015-04-10 17:02:31 -0400

    - Add logic to check if SQLCipher JVM parameter is enabled.  If enabled, run test cases under sync/datastore package.

--- sync-core/src/test/java/com/cloudant/sync/datastore/DatastoreTestBase.java
+++ sync-core/src/test/java/com/cloudant/sync/datastore/DatastoreTestBase.java
@@ -14,8 +14,9 @@
 
 package com.cloudant.sync.datastore;
 
-import com.cloudant.sync.sqlite.SQLDatabase;
+import com.cloudant.sync.sqlite.android.encryption.HelperKeyProvider;
 import com.cloudant.sync.util.TestUtils;
+
 import org.junit.After;
 import org.junit.Before;
 
@@ -33,7 +34,13 @@ public abstract class DatastoreTestBase {
     public void setUp() throws Exception {
         datastore_manager_dir = TestUtils.createTempTestingDir(this.getClass().getName());
         datastoreManager = new DatastoreManager(this.datastore_manager_dir);
-        datastore = (BasicDatastore)(this.datastoreManager.openDatastore(getClass().getSimpleName()));
+
+        //Open SQLCipher-based datastore if SQLCipher parameter is 'true'
+        if(Boolean.valueOf(System.getProperty("test.sqlcipher.passphrase"))) {
+            this.datastore = (BasicDatastore) (this.datastoreManager.openDatastore(getClass().getSimpleName(), new HelperKeyProvider()));
+        } else {
+            this.datastore = (BasicDatastore) (this.datastoreManager.openDatastore(getClass().getSimpleName()));
+        }
     }
 
     @After
