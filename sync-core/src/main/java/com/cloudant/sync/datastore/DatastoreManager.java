503d8b996c4187b5f1139cfe81348a4c1844b98d
Esteban Laver <emlaver@us.ibm.com>
2015-04-10 16:49:27 -0400

    - Add new constructors with additional key provider parameter for SQLCipher datastore

--- sync-core/src/main/java/com/cloudant/sync/datastore/DatastoreManager.java
+++ sync-core/src/main/java/com/cloudant/sync/datastore/DatastoreManager.java
@@ -19,11 +19,13 @@ package com.cloudant.sync.datastore;
 
 import com.cloudant.sync.notifications.DatabaseClosed;
 import com.cloudant.sync.notifications.DatabaseCreated;
-import com.cloudant.sync.notifications.DatabaseOpened;
 import com.cloudant.sync.notifications.DatabaseDeleted;
+import com.cloudant.sync.notifications.DatabaseOpened;
+import com.cloudant.sync.sqlite.android.encryption.KeyProvider;
 import com.google.common.base.Preconditions;
 import com.google.common.eventbus.EventBus;
 import com.google.common.eventbus.Subscribe;
+
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
 
@@ -31,7 +33,6 @@ import java.io.File;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -153,12 +154,46 @@ public class DatastoreManager {
     public Datastore openDatastore(String dbName) throws DatastoreNotCreatedException {
         Preconditions.checkArgument(dbName.matches(LEGAL_CHARACTERS),
                 "A database must be named with all lowercase letters (a-z), digits (0-9),"
-                  + " or any of the _$()+-/ characters. The name has to start with a"
-                  + " lowercase letter (a-z).");
+                        + " or any of the _$()+-/ characters. The name has to start with a"
+                        + " lowercase letter (a-z).");
         if (!openedDatastores.containsKey(dbName)) {
             synchronized (openedDatastores) {
                 if (!openedDatastores.containsKey(dbName)) {
                     Datastore ds = createDatastore(dbName);
+                    ds.getEventBus().register(this);
+                    openedDatastores.put(dbName, ds);
+                }
+            }
+        }
+        return openedDatastores.get(dbName);
+    }
+
+    /**
+     * <p>Opens a datastore that requires SQLCipher encryption.
+     * Key provider object contains the user defined SQLCipher key.</p>
+     *
+     * <p>This method finds the appropriate datastore file for a
+     * datastore, then initialises a {@link Datastore} object connected
+     * to that underlying storage file.</p>
+     *
+     * <p>If the datastore was successfully created and opened, a
+     * {@link DatabaseOpened DatabaseOpened}
+     * event is posted on the event bus.</p>
+     *
+     * @param dbName name of datastore to open
+     * @return {@code Datastore} with the given name
+     *
+     * @see DatastoreManager#getEventBus()
+     */
+    public Datastore openDatastore(String dbName, KeyProvider provider) throws DatastoreNotCreatedException {
+        Preconditions.checkArgument(dbName.matches(LEGAL_CHARACTERS),
+                "A database must be named with all lowercase letters (a-z), digits (0-9),"
+                        + " or any of the _$()+-/ characters. The name has to start with a"
+                        + " lowercase letter (a-z).");
+        if (!openedDatastores.containsKey(dbName)) {
+            synchronized (openedDatastores) {
+                if (!openedDatastores.containsKey(dbName)) {
+                    Datastore ds = createDatastore(dbName, provider);
                     ds.getEventBus().register(this);
                     openedDatastores.put(dbName, ds);
                 }
@@ -230,6 +265,36 @@ public class DatastoreManager {
         } catch (SQLException e) {
             throw new DatastoreNotCreatedException("Database not initialized correctly: " + dbName, e);
         } catch (DatastoreException e){
+            throw new DatastoreNotCreatedException("Datastore not initialized correctly: " + dbName, e);
+        }
+    }
+
+    /*
+    * Creates a datastore that requires SQLCipher encryption.
+    */
+    private Datastore createDatastore(String dbName, KeyProvider provider) throws DatastoreNotCreatedException {
+        try {
+            String dbDirectory = this.getDatastoreDirectory(dbName);
+            boolean dbDirectoryExist = new File(dbDirectory).exists();
+            logger.info("path: " + this.path);
+            logger.info("dbDirectory: " + dbDirectory);
+            logger.info("dbDirectoryExist: " + dbDirectoryExist);
+            // dbDirectory will created in BasicDatastore constructor
+            // if it does not exist
+
+            //Pass database directory, database name, and SQLCipher key provider
+            BasicDatastore ds = new BasicDatastore(dbDirectory, dbName, provider);
+
+            if(!dbDirectoryExist) {
+                this.eventBus.post(new DatabaseCreated(dbName));
+            }
+            eventBus.post(new DatabaseOpened(dbName));
+            return ds;
+        } catch (IOException e) {
+            throw new DatastoreNotCreatedException("Database not found: " + dbName, e);
+        } catch (SQLException e) {
+            throw new DatastoreNotCreatedException("Database not initialized correctly: " + dbName, e);
+        } catch (DatastoreException e) {
             throw new DatastoreNotCreatedException("Datastore not initialized correctly: " + dbName, e);
         }
     }
