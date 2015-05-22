0b8cebaa0f9393f3d1b525a24fb7327f44837f2e
Michael Rhodes <mike.rhodes@gmail.com>
2015-05-22 16:46:06 +0100

    Add licence headers, move Encryption classes to same package

--- sync-android/src/main/java/com/cloudant/sync/sqlite/android/encryption/EncryptedSavedAttachment.java
+++ sync-android/src/main/java/com/cloudant/sync/sqlite/android/encryption/EncryptedSavedAttachment.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2014 Cloudant, Inc. All rights reserved.
+ * Copyright (c) 2015 IBM Cloudant, Inc. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
  * except in compliance with the License. You may obtain a copy of the License at
@@ -12,7 +12,7 @@
  * and limitations under the License.
  */
 
-package com.cloudant.sync.sqlite.android;
+package com.cloudant.sync.sqlite.android.encryption;
 
 import com.cloudant.sync.datastore.Attachment;
 import com.cloudant.sync.datastore.encryption.KeyProvider;
