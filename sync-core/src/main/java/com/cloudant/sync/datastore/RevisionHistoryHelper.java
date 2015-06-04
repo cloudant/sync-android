b7a7fefdaf4ff1142bcba52709f18f3338ce2019
Tom Blench <tom.blench@cloudant.com>
2015-06-04 14:28:00 +0100

    Transmit encoding value correctly

--- sync-core/src/main/java/com/cloudant/sync/datastore/RevisionHistoryHelper.java
+++ sync-core/src/main/java/com/cloudant/sync/datastore/RevisionHistoryHelper.java
@@ -254,7 +254,9 @@ public class RevisionHistoryHelper {
                     theAtt.put("encoded_length", savedAtt.encodedLength);
                     theAtt.put("content_type", savedAtt.type);
                     theAtt.put("revpos", savedAtt.revpos);
-                    theAtt.put("encoding", savedAtt.encoding);
+                    if (savedAtt.encoding != Attachment.Encoding.Plain) {
+                        theAtt.put("encoding", savedAtt.encoding.toString().toLowerCase());
+                    }
                 } else {
                     // if the revpos of the attachment is higher than the minimum, it's a stub
                     theAtt.put("stub", true);
