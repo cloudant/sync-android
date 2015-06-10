8a2366c9a921d842ee71e5257f84cff2f4224ca7
Michael Rhodes <mike.rhodes@gmail.com>
2015-06-10 13:43:30 +0100

    Don't use encryption in testing based on envvar
    This commit removes the test.sqlcipher.passphrase env var
    which enabled encryption on all tests descending from
    DatastoreTestBase. It was causing issues for running the
    tests on JavaSE and made it difficult to run encryption testing
    in IDEA/Studio.
    
    Instead, we'll have a specific set of tests which run on Android
    which run though some encryption basics. Ideally we'd like to run
    all tests both with and without encryption, but we can move to
    that later.

--- sync-core/src/test/java/com/cloudant/sync/datastore/DatastoreTestBase.java
+++ sync-core/src/test/java/com/cloudant/sync/datastore/DatastoreTestBase.java
@@ -14,7 +14,6 @@
 
 package com.cloudant.sync.datastore;
 
-import com.cloudant.sync.datastore.encryption.HelperSimpleKeyProvider;
 import com.cloudant.sync.util.TestUtils;
 
 import org.junit.After;
@@ -34,13 +33,8 @@ public abstract class DatastoreTestBase {
     public void setUp() throws Exception {
         datastore_manager_dir = TestUtils.createTempTestingDir(this.getClass().getName());
         datastoreManager = new DatastoreManager(this.datastore_manager_dir);
+        this.datastore = (BasicDatastore) (this.datastoreManager.openDatastore(getClass().getSimpleName()));
 
-        //Open SQLCipher-based datastore if SQLCipher parameter is 'true'
-        if(Boolean.valueOf(System.getProperty("test.sqlcipher.passphrase"))) {
-            this.datastore = (BasicDatastore) (this.datastoreManager.openDatastore(getClass().getSimpleName(), new HelperSimpleKeyProvider()));
-        } else {
-            this.datastore = (BasicDatastore) (this.datastoreManager.openDatastore(getClass().getSimpleName()));
-        }
     }
 
     @After
