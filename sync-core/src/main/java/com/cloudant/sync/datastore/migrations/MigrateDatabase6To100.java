fdacc9b5187736738e5c7ce98efea5ef669edaef
Rhys Short <rhyshort@uk.ibm.com>
2015-06-12 14:31:28 +0100

    Close cursor on schema update

--- sync-core/src/main/java/com/cloudant/sync/datastore/migrations/MigrateDatabase6To100.java
+++ sync-core/src/main/java/com/cloudant/sync/datastore/migrations/MigrateDatabase6To100.java
@@ -17,6 +17,7 @@ package com.cloudant.sync.datastore.migrations;
 import com.cloudant.sync.sqlite.ContentValues;
 import com.cloudant.sync.sqlite.Cursor;
 import com.cloudant.sync.sqlite.SQLDatabase;
+import com.cloudant.sync.util.DatabaseUtils;
 
 import org.apache.commons.codec.binary.Hex;
 
@@ -32,23 +33,28 @@ public class MigrateDatabase6To100 implements Migration {
 
     public void runMigration(SQLDatabase db) throws Exception {
 
-        db.execSQL("CREATE TABLE attachments_key_filename ( " +
-                "key TEXT UNIQUE NOT NULL, " +
-                "filename TEXT UNIQUE NOT NULL);");
-        db.execSQL("CREATE INDEX attachments_key_filename_index " +
-                "ON attachments_key_filename(key, filename);");
+        Cursor cursor = null;
+        try {
+            db.execSQL("CREATE TABLE attachments_key_filename ( " +
+                    "key TEXT UNIQUE NOT NULL, " +
+                    "filename TEXT UNIQUE NOT NULL);");
+            db.execSQL("CREATE INDEX attachments_key_filename_index " +
+                    "ON attachments_key_filename(key, filename);");
 
-        // Migrate all existing blobs into the attachments_key_filename table
-        // using the attachment key as both the key and filename
-        String sql = "SELECT key FROM attachments;";
-        Cursor c = db.rawQuery(sql, null);
-        while (c.moveToNext()) {
-            byte[] key = c.getBlob(0);
-            String hexKey = new String(Hex.encodeHex(key));
-            ContentValues cv = new ContentValues();
-            cv.put("key", hexKey);
-            cv.put("filename", hexKey);  // existing blobs retain key as filename
-            db.insert("attachments_key_filename", cv);
+            // Migrate all existing blobs into the attachments_key_filename table
+            // using the attachment key as both the key and filename
+            String sql = "SELECT key FROM attachments;";
+            cursor = db.rawQuery(sql, null);
+            while (cursor.moveToNext()) {
+                byte[] key = cursor.getBlob(0);
+                String hexKey = new String(Hex.encodeHex(key));
+                ContentValues cv = new ContentValues();
+                cv.put("key", hexKey);
+                cv.put("filename", hexKey);  // existing blobs retain key as filename
+                db.insert("attachments_key_filename", cv);
+            }
+        } finally {
+            DatabaseUtils.closeCursorQuietly(cursor);
         }
 
     }
