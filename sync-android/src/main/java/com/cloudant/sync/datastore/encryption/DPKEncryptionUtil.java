a7578ce3a0e82cd98cadc17f0857c1c8065e6afa
Tom Blench <tom.blench@cloudant.com>
2015-06-19 10:54:33 +0100

    Add comment referencing StackOverflow solution

--- sync-android/src/main/java/com/cloudant/sync/datastore/encryption/DPKEncryptionUtil.java
+++ sync-android/src/main/java/com/cloudant/sync/datastore/encryption/DPKEncryptionUtil.java
@@ -82,6 +82,7 @@ class DPKEncryptionUtil {
             InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
         Cipher aesCipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
         IvParameterSpec ivParameter = new IvParameterSpec(iv);
+        // see http://stackoverflow.com/a/11506343
         Key encryptionKey = new SecretKeySpec(key.getEncoded(),"AES");
         aesCipher.init(Cipher.ENCRYPT_MODE, encryptionKey, ivParameter);
         return aesCipher.doFinal(unencryptedBytes);
@@ -106,6 +107,7 @@ class DPKEncryptionUtil {
             InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
         Cipher aesCipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
         IvParameterSpec ivParameter = new IvParameterSpec(iv);
+        // see http://stackoverflow.com/a/11506343
         Key encryptionKey = new SecretKeySpec(key.getEncoded(),"AES");
         aesCipher.init(Cipher.DECRYPT_MODE, encryptionKey, ivParameter);
         return aesCipher.doFinal(encryptedBytes);
