21a8ade3f3b2295923f6d40e0f4578ef408cc440
Tom Blench <tom.blench@cloudant.com>
2016-01-27 14:47:58 +0000

    Avoid excessive database traffic when doing pull replications
    Remove calls like `getAllRevisionsOfDocumentInQueue` and
    `getDocumentInQueue` which incur an un-necessarily large overhead by
    retrieving more data than is needed (especially document bodies) and
    building temporary objects which contain large amounts of data which
    are never used.
    
    Instead we make direct SQL lookups on the data needed which is quicker
    and also makes the code more compact.
    
    Batch up insertions in one SQL transaction
    
    Modify the method flow:
    `processOneChangesBatch -> bulkInsert -> forceInsert`
    to deal with batches of items to be inserted into the database.
    
    Despite the misleading names, these methods dealt with one revision at
    a time.
    
    By submitting one batch of INSERTs to the SQL database in a
    transaction, we significantly lower the overheads of insertion,
    resulting in much quicker replication times.

--- cloudant-sync-datastore-core/src/main/java/com/cloudant/sync/replication/BasicPullStrategy.java
+++ cloudant-sync-datastore-core/src/main/java/com/cloudant/sync/replication/BasicPullStrategy.java
@@ -268,6 +268,18 @@ class BasicPullStrategy implements ReplicationStrategy {
         logger.info(msg);
     }
 
+    public class BatchItem {
+
+        public BatchItem(DocumentRevsList revsList,
+                         HashMap<String[], List<PreparedAttachment>> attachments) {
+            this.revsList = revsList;
+            this.attachments = attachments;
+        }
+
+        public HashMap<String[], List<PreparedAttachment>> attachments;
+        public DocumentRevsList revsList;
+    }
+
     private int processOneChangesBatch(ChangesResultWrapper changeFeeds)
             throws ExecutionException, InterruptedException, DocumentException {
         String feed = String.format(
@@ -285,6 +297,9 @@ class BasicPullStrategy implements ReplicationStrategy {
         // Process the changes in batches
         List<String> ids = Lists.newArrayList(missingRevisions.keySet());
         List<List<String>> batches = Lists.partition(ids, this.insertBatchSize);
+
+        List<BatchItem> batchesToInsert = new ArrayList<BatchItem>();
+
         for (List<String> batch : batches) {
 
             if (this.state.cancel) { break; }
@@ -374,9 +389,10 @@ class BasicPullStrategy implements ReplicationStrategy {
                     if (this.state.cancel)
                         break;
 
-                    this.targetDb.bulkInsert(revsList, atts, this.pullAttachmentsInline);
+                    batchesToInsert.add(new BatchItem(revsList, atts));
                     changesProcessed++;
                 }
+                this.targetDb.bulkInsert(batchesToInsert, this.pullAttachmentsInline);
             } catch (Exception e) {
                 throw new ExecutionException(e);
             }
