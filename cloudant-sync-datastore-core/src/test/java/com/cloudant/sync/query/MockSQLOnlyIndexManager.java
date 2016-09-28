b91c9910ff52673ea21d097268a561811f45be5f
Tom Blench <tblench@uk.ibm.com>
2016-09-28 17:22:12 +0100

    Temp commit - new query/index API
    Some build breakage
    
    Conflicts:
    	cloudant-sync-datastore-android-encryption/src/test/java/com/cloudant/sync/datastore/encryption/EndToEndEncryptionTest.java
    	cloudant-sync-datastore-core/src/main/java/com/cloudant/sync/datastore/CloudantSync.java
    	cloudant-sync-datastore-core/src/main/java/com/cloudant/sync/query/IndexCreator.java
    	cloudant-sync-datastore-core/src/main/java/com/cloudant/sync/query/IndexManager.java
    	cloudant-sync-datastore-core/src/main/java/com/cloudant/sync/query/IndexManagerImpl.java
    	cloudant-sync-datastore-core/src/test/java/com/cloudant/sync/replication/PullStrategyTest.java

--- cloudant-sync-datastore-core/src/test/java/com/cloudant/sync/query/MockSQLOnlyIndexManager.java
+++ cloudant-sync-datastore-core/src/test/java/com/cloudant/sync/query/MockSQLOnlyIndexManager.java
@@ -43,9 +43,7 @@ public class MockSQLOnlyIndexManager extends IndexManagerImpl {
             return null;
         }
 
-        if (!updateAllIndexes()) {
-            return null;
-        }
+        updateAllIndexes();
 
         MockSQLOnlyQueryExecutor queryExecutor = null;
         try {
@@ -54,7 +52,7 @@ public class MockSQLOnlyIndexManager extends IndexManagerImpl {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-        Map<String, Object> indexes = listIndexes();
+        Map<String, Map<String, Object>> indexes = listIndexes();
         return queryExecutor.find(query, indexes, skip, limit, fields, sortDocument);
     }
 }
--- cloudant-sync-datastore-core/src/test/java/com/cloudant/sync/replication/PullStrategyTest.java
+++ cloudant-sync-datastore-core/src/test/java/com/cloudant/sync/replication/PullStrategyTest.java
@@ -27,6 +27,7 @@ import com.cloudant.mazha.Response;
 import com.cloudant.sync.datastore.DatabaseImpl;
 import com.cloudant.sync.datastore.DocumentRevision;
 import com.cloudant.sync.datastore.DocumentRevisionTree;
+import com.cloudant.sync.query.FieldSort;
 import com.cloudant.sync.query.IndexManagerImpl;
 import com.cloudant.sync.query.QueryResult;
 
@@ -387,7 +388,7 @@ public class PullStrategyTest extends ReplicationTestBase {
 
         IndexManagerImpl im = new IndexManagerImpl(datastore);
         try {
-            im.ensureIndexed(Arrays.<Object>asList("diet"), "diet");
+            im.ensureIndexed(Arrays.<FieldSort>asList(new FieldSort("diet")), "diet");
 
             AnimalDb.populateWithoutFilter(remoteDb.couchClient);
             this.pull(replicator, 10);
