b3701031911b52e9d224dab1e6ec2366aa51b548
Tom Blench <tblench@uk.ibm.com>
2016-09-29 13:36:21 +0100

    Implement toString/equals/hashCode

--- cloudant-sync-datastore-core/src/main/java/com/cloudant/sync/query/FieldSort.java
+++ cloudant-sync-datastore-core/src/main/java/com/cloudant/sync/query/FieldSort.java
@@ -19,11 +19,42 @@ public class FieldSort {
         this.sort = sort;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        FieldSort fieldSort = (FieldSort) o;
+
+        if (!field.equals(fieldSort.field)) {
+            return false;
+        }
+        return sort == fieldSort.sort;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = field.hashCode();
+        result = 31 * result + sort.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "FieldSort{" +
+                "field='" + field + '\'' +
+                ", sort=" + sort +
+                '}';
+    }
 
     public enum Direction {
         ASCENDING,
         DESCENDING
     };
-
 
 }
