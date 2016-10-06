ffe9298c700805be3fa7fdbc3ad11ebf4fab5892
Tom Blench <tblench@uk.ibm.com>
2016-10-06 11:49:08 +0100

    Implement feedback from rhys' comments

--- cloudant-sync-datastore-core/src/main/java/com/cloudant/sync/query/FieldSort.java
+++ cloudant-sync-datastore-core/src/main/java/com/cloudant/sync/query/FieldSort.java
@@ -1,3 +1,17 @@
+/*
+ * Copyright (C) 2016 IBM Corp. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+
 package com.cloudant.sync.query;
 
 /**
@@ -10,8 +24,7 @@ public class FieldSort {
     public final Direction sort;
 
     public FieldSort(String field) {
-        this.field = field;
-        this.sort = Direction.ASCENDING;
+        this(field, Direction.ASCENDING);
     }
 
     public FieldSort(String field, Direction sort) {
