5febb93794087edef4bfbc20816d25a2d9083d05
Tom Blench <tblench@uk.ibm.com>
2016-11-11 11:31:32 +0000

    Licence normalisation
    Fix typos/spacing

--- cloudant-sync-datastore-core/src/main/java/com/cloudant/sync/documentstore/encryption/CachingKeyProvider.java
+++ cloudant-sync-datastore-core/src/main/java/com/cloudant/sync/documentstore/encryption/CachingKeyProvider.java
@@ -11,6 +11,7 @@
  * either express or implied. See the License for the specific language governing permissions
  * and limitations under the License.
  */
+
 package com.cloudant.sync.documentstore.encryption;
 
 import java.util.logging.Logger;
