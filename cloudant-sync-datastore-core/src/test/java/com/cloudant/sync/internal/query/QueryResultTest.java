739c1efd5be3a999b4fdb8dfe8d718263a8c3a33
Tom Blench <tblench@uk.ibm.com>
2016-11-11 10:54:45 +0000

    Licence normalisation
    - Use © symbol everywhere
    - Remove <p> and <p/> marks
    - Remove extraneous space after * in comments

--- cloudant-sync-datastore-core/src/test/java/com/cloudant/sync/internal/query/QueryResultTest.java
+++ cloudant-sync-datastore-core/src/test/java/com/cloudant/sync/internal/query/QueryResultTest.java
@@ -1,4 +1,4 @@
-//  Copyright (C) 2016 IBM Cloudant. All rights reserved.
+//  Copyright © 2016 IBM Cloudant. All rights reserved.
 //
 //  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
 //  except in compliance with the License. You may obtain a copy of the License at
