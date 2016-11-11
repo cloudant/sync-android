9cad1e5e421af4b1be10d008a2866d0520b16494
Tom Blench <tblench@uk.ibm.com>
2016-11-11 10:54:45 +0000

    Licence normalisation
    - Use © symbol everywhere
    - Remove <p> and <p/> marks
    - Remove extraneous space after * in comments

--- cloudant-sync-datastore-core/src/main/java/com/cloudant/sync/internal/android/ContentValues.java
+++ cloudant-sync-datastore-core/src/main/java/com/cloudant/sync/internal/android/ContentValues.java
@@ -1,14 +1,14 @@
 /**
  * Modifications allow class to work outside Android framework by
- * Cloudant, Inc., Copyright (C) 2013 Cloudant, Inc.
+ * Cloudant, Inc., Copyright © 2013 Cloudant, Inc.
  *
- * Copyright (C) 2007 The Android Open Source Project
+ * Copyright © 2007 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
