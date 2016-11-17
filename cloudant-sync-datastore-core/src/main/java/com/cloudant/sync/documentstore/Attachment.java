d15353a070cf6d9faf9bf781dc65619e36e7a559
Rhys Short <rhyshort@uk.ibm.com>
2016-11-17 11:51:16 +0000

    Fix Bad Practise findbugs warnings

--- cloudant-sync-datastore-core/src/main/java/com/cloudant/sync/documentstore/Attachment.java
+++ cloudant-sync-datastore-core/src/main/java/com/cloudant/sync/documentstore/Attachment.java
@@ -87,4 +87,32 @@ public abstract class Attachment implements Comparable<Attachment>{
         }
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Attachment that = (Attachment) o;
+
+        if (name != null ? !name.equals(that.name) : that.name != null) {
+            return false;
+        }
+        if (type != null ? !type.equals(that.type) : that.type != null) {
+            return false;
+        }
+        return encoding == that.encoding;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (type != null ? type.hashCode() : 0);
+        result = 31 * result + (encoding != null ? encoding.hashCode() : 0);
+        return result;
+    }
 }
