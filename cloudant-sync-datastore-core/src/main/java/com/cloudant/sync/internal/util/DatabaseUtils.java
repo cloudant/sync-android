4329146bdeefcc77a710360aa35fc61ccbe33517
Rhys Short <rhyshort@uk.ibm.com>
2016-12-13 16:29:33 +0000

    Throw IllegalArgumentException instead of RuntimeException.

--- cloudant-sync-datastore-core/src/main/java/com/cloudant/sync/internal/util/DatabaseUtils.java
+++ cloudant-sync-datastore-core/src/main/java/com/cloudant/sync/internal/util/DatabaseUtils.java
@@ -38,17 +38,13 @@ public  class DatabaseUtils {
     }
 
     public static String makePlaceholders(int len) {
-        if (len < 1) {
-            // It will lead to an invalid query anyway ..
-            throw new RuntimeException("No placeholders");
-        } else {
-            StringBuilder sb = new StringBuilder(len * 2 - 1);
-            sb.append("?");
-            for (int i = 1; i < len; i++) {
-                sb.append(",?");
-            }
-            return sb.toString();
+        Misc.checkArgument(len >= 1, "len needs to be greater than or equal to 1.");
+        StringBuilder sb = new StringBuilder(len * 2 - 1);
+        sb.append("?");
+        for (int i = 1; i < len; i++) {
+            sb.append(",?");
         }
+        return sb.toString();
     }
 
 }
