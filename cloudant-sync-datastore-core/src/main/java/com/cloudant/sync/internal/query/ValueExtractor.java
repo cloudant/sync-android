546d0b3e81b2152f74527e1bf1da9c3c1dbfcae3
Rhys Short <rhyshort@uk.ibm.com>
2017-01-05 16:56:31 +0000

    Extract UpdateIndexCallable
    Extract UpdateIndexCallable from IndexUpdater into a standalone class, bringing with
    it methods only used by the callable in IndexUpdater, decoupling the callable from
    the IndexUpdater.

--- cloudant-sync-datastore-core/src/main/java/com/cloudant/sync/internal/query/ValueExtractor.java
+++ cloudant-sync-datastore-core/src/main/java/com/cloudant/sync/internal/query/ValueExtractor.java
@@ -25,7 +25,7 @@ import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-class ValueExtractor {
+public class ValueExtractor {
 
     private static final Logger logger = Logger.getLogger(ValueExtractor.class.getName());
 
