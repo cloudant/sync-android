3330eea95d9a105817e2c266242f02d57f01056f
Rhys Short <rhyshort@uk.ibm.com>
2017-01-05 13:23:34 +0000

    Extract ListIndexes as a standalone callable.
    Extract ListIndexes as a standalone callable and remove the duplicate
    callable from IndexCreator.

--- cloudant-sync-datastore-core/src/main/java/com/cloudant/sync/internal/query/QueryImpl.java
+++ cloudant-sync-datastore-core/src/main/java/com/cloudant/sync/internal/query/QueryImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2014, 2016 IBM Corp. All rights reserved.
+ * Copyright © 2014, 2017 IBM Corp. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
  * except in compliance with the License. You may obtain a copy of the License at
@@ -18,23 +18,20 @@ import com.cloudant.sync.documentstore.Database;
 import com.cloudant.sync.documentstore.encryption.KeyProvider;
 import com.cloudant.sync.internal.documentstore.DatabaseImpl;
 import com.cloudant.sync.internal.documentstore.migrations.SchemaOnlyMigration;
+import com.cloudant.sync.internal.query.callables.ListIndexesCallable;
 import com.cloudant.sync.query.FieldSort;
 import com.cloudant.sync.query.Index;
 import com.cloudant.sync.query.IndexType;
 import com.cloudant.sync.query.Query;
 import com.cloudant.sync.query.QueryException;
 import com.cloudant.sync.query.QueryResult;
-import com.cloudant.sync.internal.sqlite.Cursor;
 import com.cloudant.sync.internal.sqlite.SQLCallable;
 import com.cloudant.sync.internal.sqlite.SQLDatabase;
 import com.cloudant.sync.internal.sqlite.SQLDatabaseQueue;
-import com.cloudant.sync.internal.util.DatabaseUtils;
-import com.cloudant.sync.internal.util.JSONUtils;
 import com.cloudant.sync.internal.util.Misc;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.charset.Charset;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -147,53 +144,6 @@ public class QueryImpl implements Query {
             throw new QueryException(msg, e);
         }
 
-    }
-
-    protected static List<Index> listIndexesInDatabase(SQLDatabase db) throws SQLException {
-        // Accumulate indexes and definitions into a map
-        String sqlIndexNames = String.format("SELECT DISTINCT index_name FROM %s", INDEX_METADATA_TABLE_NAME);
-        Cursor cursorIndexNames = null;
-        ArrayList<Index> indexes = new ArrayList<Index>();
-        try {
-            cursorIndexNames = db.rawQuery(sqlIndexNames, new String[]{});
-            while (cursorIndexNames.moveToNext()) {
-                String indexName = cursorIndexNames.getString(0);
-                String sqlIndexes = String.format("SELECT index_type, field_name, index_settings FROM %s "+
-                        "WHERE index_name = ?",
-                        INDEX_METADATA_TABLE_NAME);
-                Cursor cursorIndexes = null;
-                try {
-                    cursorIndexes = db.rawQuery(sqlIndexes, new String[]{indexName});
-                    IndexType indexType = null;
-                    String settings = null;
-                    ArrayList<FieldSort> fieldNames = new ArrayList<FieldSort>();
-                    boolean first = true;
-                    while (cursorIndexes.moveToNext()) {
-                        if (first) {
-                            // first time round
-                            indexType = IndexType.enumValue(cursorIndexes.getString(0));
-                            settings = cursorIndexes.getString(2);
-                            first = false;
-                        }
-                        fieldNames.add(new FieldSort(cursorIndexes.getString(1)));
-                    }
-                    Map<String, Object> settingsMap = JSONUtils.deserialize(settings.getBytes(Charset.forName("UTF-8")));
-                    if (settingsMap.containsKey("tokenize") && settingsMap.get("tokenize") instanceof String) {
-                        indexes.add(new Index(fieldNames, indexName, indexType, (String)settingsMap.get("tokenize")));
-                    } else {
-                        indexes.add(new Index(fieldNames, indexName, indexType));
-                    }
-                } finally {
-                    DatabaseUtils.closeCursorQuietly(cursorIndexes);
-                }
-
-
-            }
-        } finally {
-            DatabaseUtils.closeCursorQuietly(cursorIndexNames);
-        }
-
-        return indexes;
     }
 
     /**
@@ -370,13 +320,6 @@ public class QueryImpl implements Query {
         return database;
     }
 
-
-    private static class ListIndexesCallable implements SQLCallable<List<Index>> {
-        @Override
-        public List<Index> call(SQLDatabase database) throws Exception {
-             return QueryImpl.listIndexesInDatabase(database);
-        }
-    }
 
     private static class DeleteIndexCallable implements SQLCallable<Void> {
         private final String indexName;
---
+++ cloudant-sync-datastore-core/src/main/java/com/cloudant/sync/internal/query/callables/ListIndexesCallable.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright © 2017 IBM Corp. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ *  except in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ */
+
+package com.cloudant.sync.internal.query.callables;
+
+import com.cloudant.sync.internal.query.QueryImpl;
+import com.cloudant.sync.internal.sqlite.Cursor;
+import com.cloudant.sync.internal.sqlite.SQLCallable;
+import com.cloudant.sync.internal.sqlite.SQLDatabase;
+import com.cloudant.sync.internal.util.DatabaseUtils;
+import com.cloudant.sync.internal.util.JSONUtils;
+import com.cloudant.sync.query.FieldSort;
+import com.cloudant.sync.query.Index;
+import com.cloudant.sync.query.IndexType;
+
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Lists the indexes for the database.
+ */
+public class ListIndexesCallable implements SQLCallable<List<Index>> {
+
+    @Override
+    public List<Index> call(SQLDatabase db) throws Exception {
+        // Accumulate indexes and definitions into a map
+        String sqlIndexNames = String.format("SELECT DISTINCT index_name FROM %s", QueryImpl.INDEX_METADATA_TABLE_NAME);
+        Cursor cursorIndexNames = null;
+        ArrayList<Index> indexes = new ArrayList<Index>();
+        try {
+            cursorIndexNames = db.rawQuery(sqlIndexNames, new String[]{});
+            while (cursorIndexNames.moveToNext()) {
+                String indexName = cursorIndexNames.getString(0);
+                String sqlIndexes = String.format("SELECT index_type, field_name, index_settings FROM %s " +
+                                "WHERE index_name = ?",
+                        QueryImpl.INDEX_METADATA_TABLE_NAME);
+                Cursor cursorIndexes = null;
+                try {
+                    cursorIndexes = db.rawQuery(sqlIndexes, new String[]{indexName});
+                    IndexType indexType = null;
+                    String settings = null;
+                    ArrayList<FieldSort> fieldNames = new ArrayList<FieldSort>();
+                    boolean first = true;
+                    while (cursorIndexes.moveToNext()) {
+                        if (first) {
+                            // first time round
+                            indexType = IndexType.enumValue(cursorIndexes.getString(0));
+                            settings = cursorIndexes.getString(2);
+                            first = false;
+                        }
+                        fieldNames.add(new FieldSort(cursorIndexes.getString(1)));
+                    }
+                    Map<String, Object> settingsMap = JSONUtils.deserialize(settings.getBytes(Charset.forName("UTF-8")));
+                    if (settingsMap.containsKey("tokenize") && settingsMap.get("tokenize") instanceof String) {
+                        indexes.add(new Index(fieldNames, indexName, indexType, (String) settingsMap.get("tokenize")));
+                    } else {
+                        indexes.add(new Index(fieldNames, indexName, indexType));
+                    }
+                } finally {
+                    DatabaseUtils.closeCursorQuietly(cursorIndexes);
+                }
+            }
+        } finally {
+            DatabaseUtils.closeCursorQuietly(cursorIndexNames);
+        }
+
+        return indexes;
+    }
+
+}
