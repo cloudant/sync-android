2b7f34a532bc6e7350c5f29a5d3bc39233d13fde
Tom Blench <tblench@uk.ibm.com>
2017-08-30 15:33:34 +0100

    Copyright headers

--- cloudant-sync-datastore-core/src/main/java/com/cloudant/sync/internal/query/callables/PurgeCallable.java
+++ cloudant-sync-datastore-core/src/main/java/com/cloudant/sync/internal/query/callables/PurgeCallable.java
@@ -1,14 +1,26 @@
+/*
+ * Copyright © 2017 IBM Corp. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ *  except in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ */
+
 package com.cloudant.sync.internal.query.callables;
 
-import com.cloudant.sync.internal.documentstore.DatabaseImpl;
 import com.cloudant.sync.internal.query.QueryConstants;
 import com.cloudant.sync.internal.query.QueryImpl;
 import com.cloudant.sync.internal.sqlite.Cursor;
 import com.cloudant.sync.internal.sqlite.SQLCallable;
 import com.cloudant.sync.internal.sqlite.SQLDatabase;
-import com.cloudant.sync.query.Index;
 
-import java.util.ArrayList;
 import java.util.logging.Logger;
 
 /**
